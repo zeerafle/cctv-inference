04ca12f16900f80050661a1467dbf504824b6577
Sam Fareez <vsefareez@gmail.com>
2024-06-08 23:42:47 +0800

    feat: monitor resource usage

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.utils.prediction import predict
+from app.utils.prediction import predict, predict_frame
 
 import cv2
 import json
 
-cap = {}
+import psutil
+
 app = FastAPI()
+
+cap = {}
+origins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Function to initialize the video capture object
@@ -28,3 +44,29 @@ def main():
 @app.get("/prediction/sse/{cctv_id}")
 def read_sse(cctv_id: str):
     return StreamingResponse(predict(cap[cctv_id]), media_type="text/event-stream")
+
+
+@app.get("/monitor/idle")
+def monitor_idle():
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    return {"CPU Usage": cpu_usage, "RAM Usage": ram_usage}
+
+
+def simulate_request(cap, num_frames=10):
+    for _ in range(num_frames):
+        ret, frame = cap.read()
+        if not ret:
+            continue
+        result = predict_frame(frame)
+        print(result)
+
+
+@app.get("/monitor/request")
+def monitor_request():
+    # Simulate a request by predicting a certain number of frames
+    simulate_request(cap["simpang-antasari-siradj-salman"])
+
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    return {"CPU Usage": cpu_usage, "RAM Usage": ram_usage}
